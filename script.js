9ce7269b4209ab2235e6c3e63dff38112b4312e3
Alex <kozh.aleksey@yandex.ru>
2019-07-30 23:28:46 +0400

    Arrow functions

--- script.js
+++ script.js
@@ -2,24 +2,22 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
 
 
 
-(function () {
+(() => {
     let position = 0;
-    let minPos = 0;
-    let container = document.querySelector('.slider__container');
-    let buttonRight = document.querySelector('.slider__control_right');
-    let buttonLeft = document.querySelector('.slider__control_left');
-    let headerDate = document.querySelector('.location');
-    let date = (Date.now()-(24*60*60*1000));
+    const minPos = 0;
+    const container = document.querySelector('.slider__container');
+    const buttonRight = document.querySelector('.slider__control_right');
+    const buttonLeft = document.querySelector('.slider__control_left');
+    const headerDate = document.querySelector('.location');
+    //let date = (Date.now()-(24*60*60*1000));
     buttonLeft.disabled = true;
-    let dateFormatter = new Intl.DateTimeFormat('ru', {month: "long", day: "numeric"});
-    let dayFormatter = new Intl.DateTimeFormat('ru', {weekday: "long"});
+    const dateFormatter = new Intl.DateTimeFormat('ru', {month: "long", day: "numeric"});
+    const dayFormatter = new Intl.DateTimeFormat('ru', {weekday: "long"});
+
     let dayStart = new Date();
     dayStart.setHours(0,0,0,0);
 
-
-    const daysFilter = function(dailyInfo) {
-        return dailyInfo.date >= dayStart.getTime();
-    };
+    const daysFilter = ((dailyInfo) => dailyInfo.date >= dayStart.getTime());
 
     const filteredDays = dataFile.filter(daysFilter);
 
@@ -27,18 +25,17 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
 
     let maxPos = -sliderItemWidth*(filteredDays.length-4);
 
-
-    const sliderRight = function() {
+    const sliderRight = () => {
         position = position - sliderItemWidth;
         container.style.transform = 'translateX(' + position + 'px)';
     };
 
-    const sliderLeft = function() {
+    const sliderLeft = () => {
         position = position + sliderItemWidth;
         container.style.transform = 'translateX(' + position + 'px)';
     };
 
-    buttonRight.addEventListener('click', function() {
+    buttonRight.addEventListener('click', () => {
         sliderRight();
         if (position < minPos) {
             buttonLeft.disabled = false;
@@ -47,7 +44,7 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
             buttonRight.disabled = true;
         }
     });
-    buttonLeft.addEventListener('click', function() {
+    buttonLeft.addEventListener('click', () => {
         sliderLeft();
         if (position >= minPos) {
             buttonLeft.disabled = true;
@@ -57,11 +54,11 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
         }
     });
 
-    const headerFiller = function() {
+    const headerFiller = () => {
         headerDate.innerHTML = 'Самара, ' + dateFormatter.format(filteredDays[0].date) + ', ' + dayFormatter.format(filteredDays[0].date);
     };
 
-    const slideCreator = function() {
+    const slideCreator = () => {
         for (let i = 0; i < filteredDays.length; i++) {
             newSlide = document.createElement("div");
             newSlide.setAttribute('class', 'slider__item');
@@ -72,7 +69,7 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
 
     container.style.width = (256*filteredDays.length) + 'px';
 
-    const itemCreator = function() {
+    const itemCreator = () => {
         sliderItem = document.querySelectorAll('.slider__item');
         for (let i = 0; i < sliderItem.length; i++) {
             newDay = document.createElement('div');
@@ -101,7 +98,7 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
     };
     itemCreator();
 
-    const itemFiller = function() {
+    const itemFiller = () => {
         dayList = document.querySelectorAll('.item__day');
         dateList = document.querySelectorAll('.item__date');
         tDayList = document.querySelectorAll('.item__temp-day');
@@ -109,7 +106,6 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
         cloudList = document.querySelectorAll('.item__cloud');
         rainList = document.querySelectorAll('.item__rain');
         for (let i = 0; i < sliderItem.length; i++) {
-            newImg = document.createElement('img');
             if (i !== 0) {
                 dayList[i].innerHTML = dayFormatter.format(filteredDays[i].date);
             } else {
@@ -120,6 +116,7 @@ const dataFile =     [{"date":1564344000000,"temperature":{"night":5,"day":26},"
             tNightList[i].innerHTML = 'ночью +' + filteredDays[i].temperature.night + '°';
             cloudList[i].innerHTML = filteredDays[i].cloudiness + ',';
 
+            newImg = document.createElement('img');
             if (filteredDays[i].rain === true) {
                 newImg.setAttribute("src", "images/Rain2.png");
                 sliderItem[i].insertBefore(newImg, tDayList[i]);
